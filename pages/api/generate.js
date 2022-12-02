4c70d632c3b85175de2d2ae6eab0d8aa2e02c1b1
Heizal Nagginda <patriciaheizal@gmail.com>
2022-12-02 13:58:43 +0100

    set up gpt-3 writer

---
+++ pages/api/generate.js
@@ -0,0 +1,58 @@
+// this is where we'll write our backend code to call our API
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Add our base prompt here - this makes the AI more accurate in what to output to the user
+const basePromptPrefix = 
+`
+Answer my climate related question in Paul Graham style. Please make sure the answer is not too scientific, well researched and is applicable to my question.
+
+Question:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 500,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // // build prompt two
+  // const secondPrompt =
+  // `
+  // Get the answer of my climate related question and provide further resources to further explore my area of questioning.
+
+  // Question: ${req.body.userInput}
+
+  // Answer: ${basePromptOutput.text}
+
+  // Further reading:
+  // `
+  // // I call the OpenAI API a second time with Prompt #2
+  // const secondPromptCompletion = await openai.createCompletion({
+  //   model: 'text-davinci-003',
+  //   prompt: `${secondPrompt}`,
+  //   // I set a higher temperature for this one. Up to you!
+  //   temperature: 0.85,
+	// 	// I also increase max_tokens.
+  //   max_tokens: 1250,
+  // });
+
+  // // Get the output
+  // const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,79 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (event) =>{
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Atara|AI-driven Climate Education</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Climate Education with Atara</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ask a question about the climate crisis, Atara will do the rest!</h2>
+          </div>
+          {/* Add text area*/}
+          <div className="prompt-container">
+            <textarea 
+              placeholder="Ask Atara you're question" className="prompt-box" 
+              value = {userInput}
+              onChange = {onUserChangedText} 
+            />
+            {/* Add button to generate magic text*/}
+            {/* Add loading state here - isGenraating = true if waiting / = false if not */}
+            <div className="prompt-buttons">
+              <a className={isGenerating ? 'generate-button loading' : 'generate-button'} onClick={callGenerateEndpoint}>
+                <div className="generate">
+                {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+                </div>
+              </a>
+            </div>
+            {/* Add GPT-3 output to our UI here */}
+            {apiOutput && (
+              <div className="output">
+                <div className="output-header-container">
+                  <div className="output-header">
+                    <h3>Answer</h3>
+                  </div>
+                </div>
+                {/* Display the output here using apiOutput as called above */}
+                <div className="output-content">
+                  <p>{apiOutput}</p>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -25,8 +84,8 @@ const Home = () => {
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            {/* <Image src={buildspaceLogo} alt="buildspace logo" /> */}
+            <p>Powered by Climate Operation</p>
           </div>
         </a>
       </div>

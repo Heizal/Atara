c9cc0fb2e25abca06b64c8d97b254181e254778d
Heizal Nagginda <patriciaheizal@gmail.com>
2022-12-08 16:03:59 +0100

    Add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
-        <meta
+        <meta property="og:title" content="Atara" key="title"/>
+        <meta property="og:description" content="AI-Driven Climate Education" key="description"/>
+        {/* <meta
           property="og:image"
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
-        />
+        /> */}
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
